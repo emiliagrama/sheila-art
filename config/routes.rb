90117fac765b1c52d1da080b9fee25d3b68e3bb4
emilia grama <emiliagrama@gmail.com>
2024-03-22 00:01:38 +0100

    route change, ul lists

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root to: "pages#home"
-  get "about", to: "pages#about"
+  get "artwork", to: "pages#artwork"
   get "biography", to: "pages#biography"
 end
